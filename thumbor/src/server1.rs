09cc30621df50b331a3aaf8f812a489953b965da
kaigedong <dongkaige@gmail.com>
2022-06-19 00:21:54 +0800

    refa: 格式化

--- thumbor/src/server1.rs
+++ thumbor/src/server1.rs
@@ -1,4 +1,4 @@
-use axum::{extract::Path, routing::get, http::StatusCode, Router};
+use axum::{extract::Path, http::StatusCode, routing::get, Router};
 use percent_encoding::percent_decode_str;
 use serde::Deserialize;
 
@@ -16,21 +16,27 @@ struct Params {
 async fn main() {
     // 初始化tracing
     tracing_subscriber::fmt::init();
-    
+
     // 构建路由
     let app = Router::new()
-    // `GET /image` 会执行generate函数，并把spec和url传递过去
-    .route("/image/:spec/:url", get(generate));
-    
+        // `GET /image` 会执行generate函数，并把spec和url传递过去
+        .route("/image/:spec/:url", get(generate));
+
     // 运行web服务器
     let addr = "127.0.0.1:3000".parse().unwrap();
     tracing::debug!("listenign on {}", addr);
-    axum::Server::bind(&addr).serve(app.into_make_service()).await.unwrap();
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
 
 // 目前我们就只把参数解析出来
-async fn generate(Path(Params{spec, url}): Path<Params>) -> Result<String, StatusCode> {
+async fn generate(Path(Params { spec, url }): Path<Params>) -> Result<String, StatusCode> {
     let url = percent_decode_str(&url).decode_utf8_lossy();
-    let spec: ImageSpec = spec.as_str().try_into().map_err(|_| StatusCode::BAD_REQUEST)?;
+    let spec: ImageSpec = spec
+        .as_str()
+        .try_into()
+        .map_err(|_| StatusCode::BAD_REQUEST)?;
     Ok(format!("url: {}\n spec: {:#?}", url, spec))
 }
