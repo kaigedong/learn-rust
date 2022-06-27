3d1727a1cba5a2c7a452fc1a07e8e11da79d0ec4
kaigedong <dongkaige@gmail.com>
2022-06-27 22:07:23 +0800

    feat: 添加连接postgres数据库

---
+++ exercises/rust-full-stack/A04-connect-db/README.md
@@ -0,0 +1,10 @@
+```sh
+# 首先在数据库中插入一些数据，参考db/db.sql
+
+# 然后对数据库进行查询
+cd db && cargo run
+```
+
+```console
+Courses = [Course { id: 1, teacher_id: 1, name: "first course", time: Some(2022-01-17T05:40:00) }]
+```
---
+++ exercises/rust-full-stack/A04-connect-db/db/db.sql
@@ -0,0 +1,27 @@
+# psql -U postgres
+
+CREATE DATABASE testdb;
+
+\l; # 查看已存在的数据库
+\c testdb; # 进入数据库
+
+drop table if exists course;
+
+create table course
+(
+    id serial primary key,
+    teacher_id INT not null,
+    name varchar(140) not null,
+    time TIMESTAMP default now()
+);
+
+insert into course
+       (id, teacher_id, name, time)
+values(1, 1, 'First course', '2022-01-17 05:40:00');
+insert into course
+       (id, teacher_id, name, time)
+values(2, 1, 'Second course', '2022-01-18 05:45:00');
+
+\d; # 查看表格是否创建成功
+\d course; # 查看表格信息
+SELECT * FROM course; # 查询表格数据
