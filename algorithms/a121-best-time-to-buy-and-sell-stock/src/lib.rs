5da29bb4ee94f4de2933f9f77cb07233a289e391
kaigedong <dongkaige@gmail.com>
2022-07-06 10:25:31 +0800

    feat: format

--- algorithms/a121-best-time-to-buy-and-sell-stock/src/lib.rs
+++ algorithms/a121-best-time-to-buy-and-sell-stock/src/lib.rs
@@ -36,7 +36,7 @@ mod tests {
     fn test_max_profile() {
         let a = vec![7, 1, 5, 3, 6, 4];
         assert_eq!(Solution::max_profit(a), 5);
-        let a = vec![7,6,4,3,1];
+        let a = vec![7, 6, 4, 3, 1];
         assert_eq!(Solution::max_profit(a), 0);
     }
 }
