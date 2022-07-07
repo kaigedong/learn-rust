2d00f776e012b1fef338462121bb775665a6e256
kaigedong <dongkaige@gmail.com>
2022-07-07 14:12:30 +0800

    feat: 添加4的幂

---
+++ algorithms/a342-power-of-four/src/lib.rs
@@ -0,0 +1,21 @@
+struct Solution;
+impl Solution {
+    #[allow(dead_code)]
+    pub fn is_power_of_four(n: i32) -> bool {
+        n.count_ones() == 1 && n.trailing_zeros() % 2 == 0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::Solution;
+
+    #[test]
+    fn it_works() {
+        assert!(Solution::is_power_of_four(1));
+        assert!(!Solution::is_power_of_four(3));
+        assert!(Solution::is_power_of_four(4));
+        assert!(!Solution::is_power_of_four(5));
+        assert!(Solution::is_power_of_four(16));
+    }
+}
